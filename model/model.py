7266c23469162b768fa6361b47fd1c012bffb447
Eddiejab <200442868+EddieJabraouti@users.noreply.github.com>
2025-12-18 16:38:33 -0500

    created class that implements the standard 3d convolution per each layer

--- model/model.py
+++ model/model.py
@@ -16,7 +16,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 ALLOWED_EXTENSIONS = {'.jpg', '.png', '.nii', ',jpeg', '.tif', '.tiff', '.webp'}
 
-class HeartSegmentationDataset(Dataset):
+class HeartSegmentationDataset(Dataset): #processing data for model input
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
@@ -45,7 +45,7 @@ class HeartSegmentationDataset(Dataset):
                     if os.path.exists(gt_path):
                         self.samples.append((img_path, gt_path))
 
-    def get_len(self):
+    def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
@@ -59,11 +59,37 @@ class HeartSegmentationDataset(Dataset):
 
         image = (image - image.mean()) / (image.std() + 1e-8) #Z score normalization - commonality in Cardiac Imagery
 
-         #add channel dims (1, H, W, S)
+        #(H, W, S) -> (S, H, W)
+        image = image.permute(2, 0, 1)
+        mask = mask.permute(2, 0, 1)
+
+         #add channel dims (1, S, H, W)
         image = image.unsqueeze(0)
 
         return image, mask
 
+    #need to use encoder decoder architecture
     #using Dice loss function
+
+#Convolution architecture to be applied to each layer
+class DoubleConv3D(net.module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = net.Sequential(
+            net.Conv3d(in_channels, out_channels, 3, padding=1), 
+            net.BatchNorm3d(out_channels), 
+            net.ReLU(inplace=True),
+
+            net.Conv3d(out_channels, in_channels, 3, padding=1), 
+            net.BatchNorm3d(out_channels),
+            net.ReLU(inplace=True)
+            )
+        
+class UNet3D(net.module):
+    def __init__(self, in_channels=1, num_classes = 4, base_filters=32):
+        super().__init__()
+
+
+
 
 
