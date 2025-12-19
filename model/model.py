2fe666ec6331a29bca394419e513d9a917a4430f
Eddiejab <200442868+EddieJabraouti@users.noreply.github.com>
2025-12-19 18:05:37 -0500

    integrated the encoder decoder class

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFile
 import nibabel as nib
 
 import torch 
-import torch.nn as net
+import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 
@@ -91,27 +91,30 @@ and force the netowrk to learn small seperation borders that we introduce betwee
 
 """
 #Convolution architecture to be applied to each layer
-class DoubleConv3D(net.Module):
+class DoubleConv3D(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv = net.Sequential(
-            net.Conv3d(in_channels, out_channels, 3, padding=1), 
-            net.BatchNorm3d(out_channels), 
-            net.ReLU(inplace=True),
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
 
-            net.Conv3d(out_channels, in_channels, 3, padding=1), 
-            net.BatchNorm3d(out_channels),
-            net.ReLU(inplace=True)
+            nn.Conv3d(out_channels, in_channels, 3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
             )
+    def forward(self, x):
+        return self.conv(x)
 
 
-class DiceLoss(net.Module):
+class DiceLoss(nn.Module):
     def __init__(self, smooth=1e-6):
         super().__init__()
+        self.smooth = smooth
 
     def forward(self, logits, targets):
         probs = torch.softmax(logits, dim=1)
-        targets = (torch.net.functional.one_hot(
+        targets = (torch.nn.functional.one_hot(
             targets, num_classes=probs.shape[1]
         ).permute(0,4,1,2,3).float())
 
@@ -123,22 +126,58 @@ class DiceLoss(net.Module):
 
 
 #Actual UNet encode/decode architecture implemented in this class
-class UNet3D(net.Module):
+class UNet3D(nn.Module):
     def __init__(self, in_channels=1, num_classes = 4, base_filters=32):
         super().__init__()
-        #encode - Multiple Convolutional layers to downsize image and extract features
 
+        #encode - Multiple Convolutional layers to downsize image and extract features
         self.enc1 = DoubleConv3D(in_channels, base_filters)
         self.enc2 = DoubleConv3D(in_channels, base_filters * 2)
         self.enc3 = DoubleConv3D(in_channels * 2, base_filters * 4)
         self.enc4 = DoubleConv3D(in_channels * 4, base_filters * 8)
 
-        self.pool = net.MaxPool3d(2)
+        self.pool = nn.MaxPool3d(2)
 
         #bottleneck
         self.bottleneck = DoubleConv3D(base_filters  * 8, base_filters * 16)
 
+        #decoder
+        self.up4 = nn.ConvTranspose3d(base_filters * 16, base_filters * 8, 2, stride=2)
+        self.dec4 = DoubleConv3D(base_filters * 16, base_filters * 8)
 
+        self.up3 = nn.ConvTranspose3d(base_filters * 8, base_filters * 4, 2, stride=2)
+        self.dec3 = DoubleConv3D(base_filters * 8, base_filters * 4)
+
+        self.up2 = nn.ConvTranspose3d(base_filters * 4, base_filters * 2, 2, stride=2)
+        self.dec2 = DoubleConv3D(base_filters * 4, base_filters * 2)
+
+        self.up1 = nn.ConvTranspose3d(base_filters * 2, base_filters, 2, stride=2)
+        self.dec1 = DoubleConv3D(base_filters *2, base_filters)
+
+        self.out = nn.Conv3d(base_filters, num_classes, kernel_size=1)
+
+    # concatenating feature map from downsampling convolution to upsampled feature map (Maintain classification and segmentation context)
+    def forward(self, x):
+        e1 = self.enc1(x)
+        e2 = self.enc2(x)
+        e3 = self.enc3(x)
+        e4 = self.enc4(x)
+
+        b = self.bottleneck(self.pool(e4))
+
+        d4 = self.up4(b)
+        d4 = self.dec4(torch.cat([d4, e4], dim=1))
+
+        d3 = self.up4(b)
+        d3 = self.dec3(torch.cat([d3, e3], dim=1))
+
+        d2 = self.up4(b)
+        d2 = self.dec2(torch.cat([d2, e2], dim=1))
+
+        d1 = self.up4(b)
+        d1 = self.dec1(torch.cat([d1, e1], dim=1))
+
+        return self.out(d1)
 
 #sanity check
 ds = HeartSegmentationDataset('database/training')
